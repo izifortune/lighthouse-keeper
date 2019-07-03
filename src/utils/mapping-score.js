45fd95423fb66354a9ca77c672f3d12982f58453
lukasz <lukasz.usarz@gmail.com>
2019-07-03 15:39:35 +0200

    Refactor src/utils/mapping-score.js

--- src/utils/mapping-score.js
+++ src/utils/mapping-score.js
@@ -1,24 +1,6 @@
 import { pipe, path, prop } from 'ramda'
 
-const reportCategories = prop('categories')
-
-export const reportCategoriesPerfScore = pipe(
-  reportCategories,
-  path(['performance', 'score'])
-)
-export const reportCategoriesPWAScore = pipe(
-  reportCategories,
-  path(['pwa', 'score'])
-)
-export const reportCategoriesAccessibilityScore = pipe(
-  reportCategories,
-  path(['accessibility', 'score'])
-)
-export const reportCategoriesBestpracticeScore = pipe(
-  reportCategories,
-  path(['best-practices', 'score'])
-)
-export const reportCategoriesSEOScore = pipe(
-  reportCategories,
-  path(['seo', 'score'])
+export const reportCategoriesScore = category => pipe(
+  prop('categories'),
+  path([category, 'score'])
 )
